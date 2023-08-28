24f89514d5b1579b7d8ce679d74f8809e8c191cb
nkwaaaa <nkwaaaa@gmail.com>
2023-08-28 11:52:53 -0300

    add react-hook-form, register and handlesubmit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
+import { useForm } from "react-hook-form"
+
 export default function App() {
+  const { register, handleSubmit } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       {/* nombre */}
       <label htmlFor="nombre">Nombre</label>
-      <input type="text" />
+      <input type="text" {...register("nombre")} />
 
       {/* correo */}
       <label htmlFor="correo">Correo</label>
-      <input type="email" />
+      <input type="email" {...register("correo")} />
 
       {/* password */}
       <label htmlFor="password">Password</label>
-      <input type="email" />
+      <input type="email" {...register("password")} />
 
       {/* confirmar password */}
       <label htmlFor="password">Confirmar password</label>
-      <input type="email" />
+      <input type="password" {...register("confirmarPassword")} />
 
       {/* fecha de nacimiento */}
       <label htmlFor="fechaNacimiento">Fecha de nacimiento</label>
-      <input type="date" />
+      <input type="date" {...register("fechaNacimiento")} />
 
       {/* pais */}
       <label htmlFor="pais">País</label>
-      <select name="" id="">
+      <select {...register("pais")}>
         <option value="mx">Mexico</option>
         <option value="co">Colombia</option>
         <option value="ar">Argentina</option>
       </select>
 
       {/* file */}
-      <label htmlFor="file">File</label>
-      <input type="file" />
+      <label htmlFor="foto">Foto de perfil</label>
+      <input type="file" {...register("foto")} />
 
       {/* terminos */}
       <label htmlFor="terminos">Acepto términos y condiciones</label>
-      <input type="checkbox" />
+      <input type="checkbox" {...register("terminos")} />
 
       <button>Enviar</button>
     </form>
